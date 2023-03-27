caa7482edb09c636e72c0ac21b74427c35b234ef
Stelios Kakoliris <stelioskakoliris@Stelioss-MacBook-Pro.local>
2023-03-27 13:16:46 +0100

    m

--- client/index.js
+++ client/index.js
@@ -1,5 +1,4 @@
-const io = require("socket.io-client");
-const socket = io("wss://poker-game-0fhr.onrender.com");
+const socket = io("https://poker-game-0fhr.onrender.com");
 const betInput = document.getElementById("betInput");
 const betBtn = document.getElementById("betBtn");
 const callBtn = document.getElementById("callBtn");
